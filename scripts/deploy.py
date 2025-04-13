1fc91af06fa6ed3556fc2fa0791ebacd53aa9781
Sayan Basu <sayan.basu1209@gmail.com>
2025-04-13 23:55:53 +0530

    Added helper and deployment scripts

---
+++ scripts/deploy.py
@@ -0,0 +1,67 @@
+from brownie import Lottery, config, network
+from scripts import helper_scripts
+from time import sleep
+
+def deploy_lottery():
+    print("Deploying Contract...")
+    account = helper_scripts.get_account()
+    Lottery.deploy(
+        helper_scripts.get_contract(config["contracts"]["ETH_USD_PRICE_FEED"]).address,
+        helper_scripts.get_contract(config["contracts"]["VRF_COORDINATOR"]).address,
+        helper_scripts.get_contract(config["contracts"]["LINK_TOKEN"]).address,
+        config["networks"][network.show_active()]["fee"],
+        config["networks"][network.show_active()]["keyHash"],
+        {
+            "from": account
+        },
+        publish_source=config["networks"][network.show_active()].get("verify", False)
+    )
+    print("Contract Deployed !")
+
+def start_lottery():
+    account = helper_scripts.get_account()
+    lottery = Lottery[-1]
+    starting_txn = lottery.startLottery (
+        {
+            "from": account
+        }
+    )
+    starting_txn.wait(1)
+
+    print("Lottery Started !")
+
+def enter_lottery():
+    account = helper_scripts.get_account()
+    lottery = Lottery[-1]
+    value = lottery.getEntranceFee() + 1000000
+    enter_txn = lottery.enter(
+        {
+           "from": account,
+           "value": value
+        }
+    )
+    enter_txn.wait(1)
+
+    print("You have successfully entered the lottery !")
+
+def end_lottery():
+    account = helper_scripts.get_account()
+    lottery = Lottery[-1]
+    # funding contract
+    fund_txn = helper_scripts.fund_contract_with_link(lottery.address)
+    fund_txn.wait(1)
+    end_txn = lottery.endLottery (
+        {
+            "from": account
+        }
+    )
+    end_txn.wait(1)
+    sleep(60)    # sleep for 1 minute to allow the chainlink node to generate the random number and call the callback function
+    print(f"The winner is {lottery.recentWinner()}")
+    print("Lottery ended successfully")
+
+def main():
+    deploy_lottery()
+    start_lottery()
+    enter_lottery()
+    end_lottery()
---
+++ scripts/helper_scripts.py
@@ -0,0 +1,96 @@
+from brownie import (
+    accounts, 
+    network, 
+    config, 
+    MockV3Aggregator, 
+    VRFCoordinatorV2Mock, 
+    LinkToken, 
+    Contract,
+    interface
+)
+
+DEVELOPMENT_ENVIRONMENTS = [
+    "development",
+    "ganache-local"
+]
+
+FORKED_ENVIRONMENTS = [
+    "mainnet-fork"
+]
+
+NAME_TO_CONTRACT_MAP = {
+    config["contracts"]["ETH_USD_PRICE_FEED"]: MockV3Aggregator,
+    config["contracts"]["VRF_COORDINATOR"]: VRFCoordinatorV2Mock,
+    config["contracts"]["LINK_TOKEN"]: LinkToken,
+}
+
+AGGREGATOR_DECIMALS = 8
+AGGREGATOR_STARTING_ANSWER = 20_000_000_000
+
+def get_account(account_index=None, account_id=None):
+    if account_index:
+        return accounts[account_index]
+    if account_id:
+        return accounts.load(account_id)
+    
+    if network.show_active() in DEVELOPMENT_ENVIRONMENTS or network.show_active() in FORKED_ENVIRONMENTS:
+        return accounts[0]
+    return accounts.add(config["wallets"]["from_key"])
+
+def get_contract(contract_name):
+    contract_type = NAME_TO_CONTRACT_MAP.get(contract_name, None)
+    if contract_type != None:
+        if network.show_active() in DEVELOPMENT_ENVIRONMENTS:
+            # If on a development network, deploy a mock on local chain if not yet mocked, else get the most recently deployed mock
+            if len(contract_type) <= 0:
+                deploy_mocks()
+            contract = contract_type[-1]
+        else:
+            # If on a testnet or mainnet, get address of already deployed contract
+            contract_address = config["networks"][network.show_active()][contract_name]
+            contract = Contract.from_abi(contract_type._name, contract_address, contract_type.abi)      # Deploy the contract using contract name, address and abi
+        return contract
+    raise ValueError(f"No contract named {contract_name} exists")
+
+def deploy_mocks(decimals=AGGREGATOR_DECIMALS, starting_ans=AGGREGATOR_STARTING_ANSWER):
+    print("Deploying Mocks...")
+    MockV3Aggregator.deploy(
+        decimals, 
+        starting_ans, 
+        {
+            "from": get_account()
+        }
+    )
+    LinkToken.deploy(
+        {
+            "from": get_account()
+        }
+    )
+    VRFCoordinatorV2Mock.deploy(
+        config["networks"][network.show_active()].get("fee"),
+        config["networks"][network.show_active()].get("gasPrice"),
+        {
+            "from": get_account()
+        }
+    )
+    print("Mocks deployment Successful !")
+
+def fund_contract_with_link(contract_address, account=None, link_token=None, amount=10 ** 17):
+    account = account if account else get_account()
+    # directly using the link token contract
+    link_token_contract = link_token if link_token else get_contract(config["contracts"]["LINK_TOKEN"])
+
+    # using the link token interface
+    # link_token_contract = interface.LinkTokenInterface(link_token.address)
+    
+    txn = link_token_contract.transfer(
+        contract_address, 
+        amount, 
+        {
+            "from": account
+        }
+    )
+    txn.wait(1)
+
+    print("Smart contract funded with LINK !")
+    return txn
